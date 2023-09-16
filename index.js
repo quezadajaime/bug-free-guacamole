9e66f178258855c4be6bea7ab33c7ab088db2413
Jaime <quezadajaime@ymail.com>
2023-09-16 13:04:51 -0700

    Node index working

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const inquirer = require('inquirer');
 
 const fs = require('fs');
 
-const { Triangle, Square, Circle } = require('./lib/shapes');
+const {Triangle, Square, Circle } = require('./lib/shapes');
 
 function writeToFile(fileName, answers) {
     let svgString = '';
 
     svgString =
-        '<svg version="1.1" width="300" height="200" xmlns="http:www.w3.org/2000/svg">';
+        '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
     svgString += '<g>';
 
     svgString += `${answers.shape}`;
@@ -16,10 +16,11 @@ function writeToFile(fileName, answers) {
     let shapeSelection;
     if (answers.shape === "Triangle") {
         shapeSelection = new Triangle(); 
+
             svgString += `<polygon points= "150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
         } else if (answers.shape === "Square") {
             shapeSelection = new Square();
-            svgString += `<rect x"73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+            svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
         } else {
             shapeSelection = new Circle();
             svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
@@ -31,7 +32,7 @@ function writeToFile(fileName, answers) {
         svgString += "</svg>";
 
         fs.writeFile(fileName, svgString, (err) => {
-            err ? console.log(err) : console.log("log.svg has been Generated, please open the .svg file");
+            err ? console.log(err) : console.log("log.svg has been Generated, please open the logo.svg file");
         });
     }
 
@@ -45,13 +46,13 @@ function writeToFile(fileName, answers) {
                 },
                 {
                     type: "input",
-                    message: "What color would you like the background of your shape to be? (Enter color keyword or a hexadecimal number)",
+                    message: "What color would you like the text to be? (Enter color keyword or a hexadecimal number)",
                     name: "textColor",
                 },
                 {
                     type: "list",
                     message: "What shape would you like your logo to be?",
-                    choices: ["Triange", "Square", "Circle"],
+                    choices: ["Triangle", "Square", "Circle"],
                     name: "shape",
                 },
                 {
@@ -65,13 +66,10 @@ function writeToFile(fileName, answers) {
                     console.log("The input can only be up to 3 characters");
                     promptUser();
                 } else {
-                    writeFile("logo.svg", answers);
+                    writeToFile("logo.svg", answers);
                 }
             });
     }
-
-
-
 
 
 
